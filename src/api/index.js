2b84c28c66d0cd906379b28b271a6a824045ec38
victoralvesf <viictor.alvess12@gmail.com>
2020-01-16 01:04:15 -0300

    fix: Removed headers from request and added in api

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,17 @@ const baseURL = 'https://cfe.sefaz.ce.gov.br:8443/portalcfews/mfe/fiscal-coupons
 
 const instance = axios.create({
   baseURL,
+  headers: {
+    'connection': 'keep-alive',
+    "accept": "application/json, text/plain, */*",
+    "origin": "http://cfe.sefaz.ce.gov.br",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+    "sec-fetch-site": "cross-site",
+    "sec-fetch-mode": "cors",
+    "referer": "http://cfe.sefaz.ce.gov.br/mfe/servicos",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,de;q=0.4"
+  },
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   })
--- src/app/ExtractController.js
+++ src/app/ExtractController.js
@@ -13,25 +13,16 @@ class ExtractController {
       })
     }
 
-    await instance.get(`/${cfeKey}`, {
-      headers: {
-        'connection': 'keep-alive',
-        "accept": "application/json, text/plain, */*",
-        "origin": "http://cfe.sefaz.ce.gov.br",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
-        "sec-fetch-site": "cross-site",
-        "sec-fetch-mode": "cors",
-        "referer": "http://cfe.sefaz.ce.gov.br/mfe/servicos",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,de;q=0.4"
-      }
-    })
-    .then(function (response) {
-      return res.json(response.data)
-    })
-    .catch(function (error) {
-      return res.status(500).json(error)
-    })
+    await instance.get(`/${cfeKey}`)
+      .then(function (response) {
+        return res.json(response.data)
+      })
+      .catch(function (error) {
+        return res.status(500).json({
+          status: false,
+          response: `Não foi possível realizar a consulta. Tente novamente mais tarde.`
+        })
+      })
   }
 }
 
